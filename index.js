6d1f49f019e73a1eddc1cc4fed9b6c18522e4cc6
Alyssa Hope <alyssa@organikseo.com>
2017-08-29 13:37:23 -0700

    bug fix

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs')
+
 var app = express();
 
 var questions = {
@@ -27,6 +30,7 @@ var questions = {
 //tell Express that well keep files in the /public directory
 //recognize this is a middleware function - as are all things in express
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // see ejs templating site http://www.embeddedjs.com
 // ejs uses erb-like <%= %> tags to execute js code
@@ -59,6 +63,26 @@ app.get('/trivia/answer/:question', function(request, response){
   var triviaQuestion = questions[questionKey];
   response.render('trivia-answer', {'question': triviaQuestion.question, 'answer': triviaQuestion.answer});
 });
+
+app.get('/form', function(request, response){
+    response.render('simple-form');
+});
+
+app.post('/day-update', function(request, response){
+  let responses = request.body
+  var dayUpdates = undefined;
+
+  //open the data.json file, and parse into a JSON Object
+  var rawFile = fs.readFileSync('data.json')
+  var dayUpdates = JSON.parse(rawFile);
+
+  dayUpdates.push(responses)
+
+  //time to save the data back to disk
+  fs.writeFileSync('data.json', JSON.stringify(dayUpdates))
+
+  response.render('day-update', { responses: dayResponses } )
+})
 
 // extra notes: Express knew to automatically look in the /views directory for our template. Notice that we left off the file extension. Express knows what type of file we are looking for because we declared EJS as our template engine above.
 
